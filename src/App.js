f76085ac3f70164fc227893f4633ae5c8459dcfc
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-06-24 20:35:33 -0300

    chore: criando rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      
+      <Router>
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
         </div>
-      
+        <Route exact path="/" element={TodoList}></Route>
+        <Route path="/create" element={CreateTodo}></Route>
+        <Route path="/edit/:id" element={EditTodo}></Route>
+      </Router>
     );
   }
 }
