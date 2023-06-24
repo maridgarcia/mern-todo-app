2b369679f304ab4dbb389a73347d28a710004d83
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-06-24 20:44:39 -0300

    chore: ajustando rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo"
+import EditTodo from "./components/EditTodo"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
@@ -7,9 +11,11 @@ class App extends Component {
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
         </div>
-        <Route exact path="/" element={TodoList}></Route>
-        <Route path="/create" element={CreateTodo}></Route>
-        <Route path="/edit/:id" element={EditTodo}></Route>
+        <Routes>
+          <Route exact path="/" element={TodoList}></Route>
+          <Route path="/create" element={CreateTodo}></Route>
+          <Route path="/edit/:id" element={EditTodo}></Route>
+        </Routes>
       </Router>
     );
   }
