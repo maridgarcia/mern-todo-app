d8e78d37e3f2a4be48d4351735fba1e690663ff0
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-06-25 13:39:22 -0300

    chore: criando conexão com mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/todos',  { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
