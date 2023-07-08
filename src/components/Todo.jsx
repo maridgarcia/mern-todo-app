f59d9b882c96e277d0e2ae11cb14fc8b3784c854
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-07-08 20:04:17 -0300

    style: todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Todo = props => (
-    <tr>
-        <td>{props.todo.task}</td>
-        <td>{props.todo.responsible}</td>
-        <td>{props.todo.priority}</td>
-        <td>
-            <Link to={"/edit/"+props.todo._id}>Edit</Link>
-        </td>
-    </tr>
+        <tr>
+            <td className={props.todo.completed ? "completed" : ''}>{props.todo.task}</td>
+            <td className={props.todo.completed ? "completed" : ''}>{props.todo.responsible}</td>
+            <td className={props.todo.completed ? "completed" : ''}>{props.todo.priority}</td>
+            <td>
+                <Link to={`/edit/${props.todo._id}`}>✏️</Link>
+            </td>
+        </tr>
 );
 
 export default Todo;
