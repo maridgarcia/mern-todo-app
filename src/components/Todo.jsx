8bf0945c598cc723f154bc6e004569365d0bbb9d
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-08-09 16:08:10 -0300

    god! essas linhas comentadas estão começando a me irritar!

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,20 +4,14 @@ import { Link } from "react-router-dom";
 export default class Todo extends React.Component {
     constructor(props) {
         super(props)
-
-        // this.state = {
-        //     todo: {}
-        // }
     }
     render() {
-        // const { todo } = this.state;
         return (
         <tr>
             <td className={this.props.todo.completed ? "completed" : ''}>{this.props.todo.task}</td>
             <td className={this.props.todo.completed ? "completed" : ''}>{this.props.todo.responsible}</td>
             <td className={this.props.todo.completed ? "completed" : ''}>{this.props.todo.priority}</td>
             <td>
-                {/* <Link to={{pathname: `/update/${this.props.todo._id}`, state: { todo }}}>✏️</Link> */}
                 <Link to={`/update/${this.props.todo._id}`}>✏️</Link>
             </td>
         </tr>
