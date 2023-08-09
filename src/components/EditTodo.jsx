e654fc115733c82d328a1d158b0cc40b6e6e297f
Mariana Garcia <mariana.garcia@beedoo.com.br>
2023-08-09 16:06:53 -0300

    apaga linha comentada

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -133,7 +133,6 @@ export default class EditTodo extends Component {
                     <br />
 
                     <div className="form-group">
-                        {/* <input type="submit" value="Update Todo" className="btn btn-primary" /> */}
                         <input type="submit" value="Update Todo" className="btn btn-primary" onClick={this.handleOnClick}/>
                     </div>
                 </form>
